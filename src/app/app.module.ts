0a7fe2cc8e315f074d918e35390278bbf4b5f144
poprotskii <stanislav.poprotskii@orbitsoft.com>
2021-09-03 09:09:34 +0300

    add books stort

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,11 @@ import { StoreModule } from '@ngrx/store'
 import { counterReducer } from './store/counter.reducer'
 import { MyCounterComponent } from './my-counter/my-counter.component'
 import { environment } from 'src/environments/environment'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { BookListComponent } from './book-list/book-list.component'
+import { BookCollectionComponent } from './book-collection/book-collection.component'
+import { booksReducer } from './state/books.reducer'
+import { collectionReducer } from './state/collection.reducer'
 
 
 @NgModule({
@@ -26,7 +31,9 @@ import { environment } from 'src/environments/environment'
     MessagesComponent,
     DashboardComponent,
     HeroSearchComponent,
-    MyCounterComponent
+    MyCounterComponent,
+    BookListComponent,
+    BookCollectionComponent
   ],
   imports: [
     BrowserModule,
@@ -36,7 +43,12 @@ import { environment } from 'src/environments/environment'
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
       dataEncapsulation: false,
     }),
-    StoreModule.forRoot({count: counterReducer}),
+    StoreModule.forRoot({count: counterReducer, books: booksReducer, collection: collectionReducer}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
     
   ],
   providers: [],
--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,10 +1,10 @@
 import { createReducer, on } from '@ngrx/store';
 import { increment, decrement, reset } from './counter.actions';
 
-export const initialState = 0;
+export const initialStateCounter = 0;
 
 const _counterReducer = createReducer(
-  initialState,
+  initialStateCounter,
   on(increment, (state) => state + 1),
   on(decrement, (state) => state - 1),
   on(reset, (state) => 0)
